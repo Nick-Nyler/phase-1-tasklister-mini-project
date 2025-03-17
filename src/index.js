2d793045ef8b733a594913c3621c58dab0af7376
Nick <nicknyler@gmail.com>
2025-03-17 15:19:06 +0300

    commit changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newTaskDescription = document.querySelector('#new-task-description').value;
+    const taskItem = document.createElement('li');
+    taskItem.textContent = newTaskDescription;
+    taskList.appendChild(taskItem);
+    form.reset();
+  });
 });
